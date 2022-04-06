e717b0e1de24e25079c055d038da8028844df7d0
Jonatas A <jonatasaraujofx@gmail.com>
2022-04-05 23:39:31 -0300

    ♻️ refactor: improve code style and docs

--- main.py
+++ main.py
@@ -15,24 +15,25 @@
 
 import os
 
+
 ########## CONSTANTES ##########
 
-# Quantidades de itens por cesta básica
+# Quantidades de itens necessários minimos em cada cesta básica
+ACUCAR_QNT_POR_CESTA = 1  # KG
+ARROZ_QNT_POR_CESTA = 4  # KG
+CAFE_QNT_POR_CESTA = 2  # KG
+EXTRATO_DE_TOMATE_QNT_POR_CESTA = 2  # UNIDADE
+MACARRAO_QNT_POR_CESTA = 3  # UNIDADE
+PCT_BOLACHA_QNT_POR_CESTA = 1  # UNIDADE
+OLEO_QNT_POR_CESTA = 1  # LITRO
+FARINHA_DE_TRIGO_QNT_POR_CESTA = 1  # KG
+FEIJAO_QNT_POR_CESTA = 4  # KG
+SAL_QNT_POR_CESTA = 1  # KG
 
-CESTA_QNT_ACUCAR = 1  # KG
-CESTA_QNT_ARROZ = 4  # KG
-CESTA_QNT_CAFE = 2  # KG
-CESTA_QNT_EXTRATO_DE_TOMATE = 2  # UNIDADE
-CESTA_QNT_MACARRAO = 3  # UNIDADE
-CESTA_QNT_PCT_BOLACHA = 1  # UNIDADE
-CESTA_QNT_OLEO = 1  # LITRO
-CESTA_QNT_FARINHA_DE_TRIGO = 1  # KG
-CESTA_QNT_FEIJAO = 4  # KG
-CESTA_QNT_SAL = 1  # KG
 
 ########## VARIÁVEIS ##########
 
-# Quantidades de itens doados
+# Quantidades de itens doados por tipo
 qnt_acucar = 0
 qnt_arroz = 0
 qnt_cafe = 0
@@ -45,7 +46,7 @@ qnt_feijao = 0
 qnt_sal = 0
 qnt_outros = 0
 
-# Quantidades de itens sobrando por cesta básica
+# Quantidades de itens que sobraram após a montagem das cestas básicas
 sobra_acucar = 0
 sobra_arroz = 0
 sobra_cafe = 0
@@ -59,9 +60,9 @@ sobra_sal = 0
 sobra_outros = 0
 
 # SOBRE A CESTA
-quantidade_de_cestas=0
-quantidade_de_cestas_com_item_extra=0
-quantidade_de_cestas_sem_item_extra=0
+quantidade_de_cestas=0 # Quantas cestas serão montadas
+quantidade_de_cestas_com_item_extra=0 # Quantas cestas receberam itens extras
+quantidade_de_cestas_sem_item_extra=0 # Quantas cestas NÃO receberam itens extras
 
 # Quantidades por tipo de pessoa (física ou juridica)
 qnt_de_doacoes_por_pessoa_fisica = 0
@@ -139,23 +140,23 @@ print("[0] Adicionar doação")
 print("[1] Ir para o relatório")
 print("[2] Sair")
 
-opcao_inicial = ler_e_validar_input_de_numeros(0, 2, "Sua Opção: ", "Digite um número entre 0 (zero) e 2 (dois): ")
+opcao_inicial = ler_e_validar_input_de_numeros(0, 2, "Sua Opção: ", "Digite um número entre 0 (zero) e 2 (dois): ") # recebe e valida a opção do usuário
 
-if opcao_inicial == 0:
+if opcao_inicial == 0: # se a opção for 0, o usuário irá adicionar uma doação
     deve_continuar = True
-elif opcao_inicial == 1:
+elif opcao_inicial == 1: # se a opção for 1, o usuário irá para o relatório
     deve_continuar = False
-else:
+else: # se a opção for 2, o usuário irá sair do programa
     deve_continuar = False
     mostrar_relatorio = False
 
-while deve_continuar:
+while deve_continuar: # enquanto o usuário não sair do programa
     clear() # limpa a tela do terminal
     print("\n=-=-=-=-=-=-=-= Sobre o doador =-=-=-=-=-=-=-=")
 
     # Recebe os dados sobre o doador
     nome = str(input("Qual o nome do doador? ")).upper() # nome do doador
-    tipo_do_doador = ler_e_validar_tipo_do_doador("Qual o tipo de pessoa? (fisica ou juridica) ")
+    tipo_do_doador = ler_e_validar_tipo_do_doador("Qual o tipo de pessoa? (fisica ou juridica) ") # recebe e valida o tipo de doador ( fisica ou juridica)
 
     # Mostra na tela os itens do menu
     print("\n=-=-=-=-=-=-=-= Escolha o item para adicionar =-=-=-=-=-=-=-=")
@@ -177,8 +178,9 @@ while deve_continuar:
         local_qnt_acucar = local_qnt_arroz = local_qnt_cafe = local_qnt_extrato_de_tomate = local_qnt_macarrao = local_qnt_pct_bolacha = local_qnt_oleo = local_qnt_farinha_de_trigo = local_qnt_feijao = local_qnt_sal = local_qnt_outros = 0
 
         # Verifica a opção que o usuário digitou
+        # Se for um tipo valido, ele pede um input de entrada e atualiza o valor da variável correspondente
         if numero_da_opcao == 0: # açucar
-            local_qnt_acucar = ler_e_validar_item(f"Quanto de Açúcar foi doado por {nome}? (KG) ")
+            local_qnt_acucar = ler_e_validar_item(f"Quanto de Açúcar foi doado por {nome}? (KG) ") # recebe e valida a quantidade do item
         elif numero_da_opcao == 1: # arroz
             local_qnt_arroz = ler_e_validar_item(f"Quanto de Arroz foi doado por {nome}? (KG) ")
         elif numero_da_opcao == 2: # café
@@ -213,9 +215,10 @@ while deve_continuar:
         qnt_sal += local_qnt_sal
         qnt_outros += local_qnt_outros
 
+        # Soma todos os itens doados numa variável local
         total_itens_doados_pelo_usuario = local_qnt_acucar + local_qnt_arroz + local_qnt_cafe + local_qnt_extrato_de_tomate + local_qnt_macarrao + local_qnt_pct_bolacha + local_qnt_oleo + local_qnt_farinha_de_trigo + local_qnt_feijao + local_qnt_sal + local_qnt_outros
 
-        # Verifica o tipo de pessoa e adiciona o item doado ao tipo
+        # Verifica o tipo de pessoa e adiciona a quantidade total doada ao tipo de pessoa ( fisica ou juridica )
         if tipo_do_doador == "fisica":
             qnt_de_doacoes_por_pessoa_fisica += total_itens_doados_pelo_usuario
         elif tipo_do_doador == "juridica":
@@ -228,13 +231,13 @@ while deve_continuar:
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
     print("[0] {:<25} [1] {:<25}".format("Adicionar novo doador", "Ir para relatórios"))
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-    deve_continuar = ler_e_validar_input_de_numeros(0, 1, "Sua opção: ", "Valor informado é inválido.") == 0
+    deve_continuar = ler_e_validar_input_de_numeros(0, 1, "Sua opção: ", "Valor informado é inválido.") == 0 # recebe e valida a opção de continuar ou não no programa
 
 
 
 ########## PRINT DOS RELATÓRIOS ##########
 
-if mostrar_relatorio:
+if mostrar_relatorio: # se foi escolhido a opção de mostrar o relatório
     clear() # limpa a tela do terminal
 
     print("\n=-=-=-=-=-=-=-= Relatório das Doações =-=-=-=-=-=-=-=\n")
@@ -257,18 +260,19 @@ if mostrar_relatorio:
 
 
     # calcula quantas cestas básicas poderão ser formadas
-    cestas_com_acucar = qnt_acucar // CESTA_QNT_ACUCAR
-    cestas_com_arroz = qnt_arroz // CESTA_QNT_ARROZ
-    cestas_com_cafe = qnt_cafe // CESTA_QNT_CAFE
-    cestas_com_extrato_de_tomate = qnt_extrato_de_tomate // CESTA_QNT_EXTRATO_DE_TOMATE
-    cestas_com_macarrao = qnt_macarrao // CESTA_QNT_MACARRAO
-    cestas_com_pct_bolacha = qnt_pct_bolacha // CESTA_QNT_PCT_BOLACHA
-    cestas_com_oleo = qnt_oleo // CESTA_QNT_OLEO
-    cestas_com_farinha_de_trigo = qnt_farinha_de_trigo // CESTA_QNT_FARINHA_DE_TRIGO
-    cestas_com_feijao = qnt_feijao // CESTA_QNT_FEIJAO
-    cestas_com_sal = qnt_sal // CESTA_QNT_SAL
+    cestas_com_acucar = qnt_acucar // ACUCAR_QNT_POR_CESTA # divide a quatidade do item doado pela quantidade necessaria deste item por cesta, retornando valor inteiro da divisão, sem resto
+    cestas_com_arroz = qnt_arroz // ARROZ_QNT_POR_CESTA
+    cestas_com_cafe = qnt_cafe // CAFE_QNT_POR_CESTA
+    cestas_com_extrato_de_tomate = qnt_extrato_de_tomate // EXTRATO_DE_TOMATE_QNT_POR_CESTA
+    cestas_com_macarrao = qnt_macarrao // MACARRAO_QNT_POR_CESTA
+    cestas_com_pct_bolacha = qnt_pct_bolacha // PCT_BOLACHA_QNT_POR_CESTA
+    cestas_com_oleo = qnt_oleo // OLEO_QNT_POR_CESTA
+    cestas_com_farinha_de_trigo = qnt_farinha_de_trigo // FARINHA_DE_TRIGO_QNT_POR_CESTA
+    cestas_com_feijao = qnt_feijao // FEIJAO_QNT_POR_CESTA
+    cestas_com_sal = qnt_sal // SAL_QNT_POR_CESTA
 
-    quantidade_de_cestas = min(cestas_com_acucar, cestas_com_arroz, cestas_com_cafe, cestas_com_extrato_de_tomate, cestas_com_macarrao, cestas_com_pct_bolacha, cestas_com_oleo, cestas_com_farinha_de_trigo, cestas_com_feijao, cestas_com_sal)
+    quantidade_de_cestas = min(cestas_com_acucar, cestas_com_arroz, cestas_com_cafe, cestas_com_extrato_de_tomate, cestas_com_macarrao, cestas_com_pct_bolacha, cestas_com_oleo, cestas_com_farinha_de_trigo, cestas_com_feijao, cestas_com_sal) # pega o menor valor dentre as cestas calculadas.
+    
     # fim do calculo
 
     print("Cestas básicas que poderão ser formadas:")
@@ -285,16 +289,16 @@ if mostrar_relatorio:
     print(f"     {quantidade_de_cestas_sem_item_extra} cesta(s) básica(s)\n")
 
     # calcula quantos itens sobraram após a montagem das cestas
-    sobra_acucar = qnt_acucar - (quantidade_de_cestas * CESTA_QNT_ACUCAR)
-    sobra_arroz = qnt_arroz - (quantidade_de_cestas * CESTA_QNT_ARROZ)
-    sobra_cafe = qnt_cafe - (quantidade_de_cestas * CESTA_QNT_CAFE)
-    sobra_extrato_de_tomate = qnt_extrato_de_tomate - (quantidade_de_cestas * CESTA_QNT_EXTRATO_DE_TOMATE)
-    sobra_macarrao = qnt_macarrao - (quantidade_de_cestas * CESTA_QNT_MACARRAO)
-    sobra_pct_bolacha = qnt_pct_bolacha - (quantidade_de_cestas * CESTA_QNT_PCT_BOLACHA)
-    sobra_oleo = qnt_oleo - (quantidade_de_cestas * CESTA_QNT_OLEO)
-    sobra_farinha_de_trigo = qnt_farinha_de_trigo - (quantidade_de_cestas * CESTA_QNT_FARINHA_DE_TRIGO)
-    sobra_feijao = qnt_feijao - (quantidade_de_cestas * CESTA_QNT_FEIJAO)
-    sobra_sal = qnt_sal - (quantidade_de_cestas * CESTA_QNT_SAL)
+    sobra_acucar = qnt_acucar - (quantidade_de_cestas * ACUCAR_QNT_POR_CESTA)
+    sobra_arroz = qnt_arroz - (quantidade_de_cestas * ARROZ_QNT_POR_CESTA)
+    sobra_cafe = qnt_cafe - (quantidade_de_cestas * CAFE_QNT_POR_CESTA)
+    sobra_extrato_de_tomate = qnt_extrato_de_tomate - (quantidade_de_cestas * EXTRATO_DE_TOMATE_QNT_POR_CESTA)
+    sobra_macarrao = qnt_macarrao - (quantidade_de_cestas * MACARRAO_QNT_POR_CESTA)
+    sobra_pct_bolacha = qnt_pct_bolacha - (quantidade_de_cestas * PCT_BOLACHA_QNT_POR_CESTA)
+    sobra_oleo = qnt_oleo - (quantidade_de_cestas * OLEO_QNT_POR_CESTA)
+    sobra_farinha_de_trigo = qnt_farinha_de_trigo - (quantidade_de_cestas * FARINHA_DE_TRIGO_QNT_POR_CESTA)
+    sobra_feijao = qnt_feijao - (quantidade_de_cestas * FEIJAO_QNT_POR_CESTA)
+    sobra_sal = qnt_sal - (quantidade_de_cestas * SAL_QNT_POR_CESTA)
     sobra_outros = max(qnt_outros - quantidade_de_cestas, 0)
     # fim do calculo
 
